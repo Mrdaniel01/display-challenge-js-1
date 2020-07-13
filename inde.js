2e2056bef1f01f399897248e6c548cd0cae3ad6a
Daniel Rios <mrdaniel19996@gmail.com>
2020-07-13 11:45:35 -0500

    initial commit

---
+++ inde.js
@@ -0,0 +1,9 @@
+function swicthLamp () {
+    var swicth = document.getElementById('swicth')
+
+    if (swicth.style.display === "block"){
+        swicth.style.display = "none"
+    }else{
+        swicth.style.display = "block"
+    }
+} 
